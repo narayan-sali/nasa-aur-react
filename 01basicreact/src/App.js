2906794483556ade0bd8fd1c6510d3c6c708b6d7
narayan-sali <narayanvs015@gmail.com>
2023-08-19 01:41:14 +0530

    created new project with react as well as vite

---
+++ 01basicreact/src/App.js
@@ -0,0 +1,5 @@
+function App() {
+  return <h1>NASA LEARNING REACT</h1>;
+}
+
+export default App;
---
+++ 01vitereact/src/App.jsx
@@ -0,0 +1,5 @@
+function App() {
+  return <h1>NASA WITH VITE REACT</h1>;
+}
+
+export default App;
